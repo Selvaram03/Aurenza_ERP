e88aa75885f39d43b4b50a2ef97640af9ccbf02c
Selva rama bala subramanian S <122852256+Selvaram03@users.noreply.github.com>
2026-02-23 18:04:36 +0530

    Delete src directory

--- src/components/StatCard.jsx
+++
@@ -1,8 +0,0 @@
-export default function StatCard({ title, value, tone = "neutral" }) {
-  return (
-    <article className={`stat-card ${tone}`}>
-      <p>{title}</p>
-      <h3>{value}</h3>
-    </article>
-  );
-}
